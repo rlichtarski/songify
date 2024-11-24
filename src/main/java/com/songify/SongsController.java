e8b376cc6494bfc4ed6bf92a1d7db8a6e957dfb1
Radosław Lichtarski <lichtarski.radek@gmail.com>
2024-11-24 18:28:07 +0100

    integrate swagger

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -1,29 +1,45 @@
 package com.songify;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
+@Log4j2
 public class SongsController {
 
     Map<Integer, String> database = new HashMap<>();
 
     @GetMapping("/songs")
-    public ResponseEntity<SongResponseDto> getAllSongs() {
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit) {
         database.put(1, "Billie Eilish");
         database.put(2, "Taco Hemingway");
+        database.put(3, "Metallica");
+        database.put(4, "AC/DC");
+        if (limit != null) {
+            Map<Integer, String> limitedSongs = database.entrySet()
+                    .stream()
+                    .limit(limit)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            SongResponseDto songResponseDto = new SongResponseDto(limitedSongs);
+            return ResponseEntity.ok(songResponseDto);
+        }
         SongResponseDto songResponseDto = new SongResponseDto(database);
         return ResponseEntity.ok(songResponseDto);
     }
 
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id) {
+    public ResponseEntity<SingleSongResponseDto> getSongById(
+            @PathVariable Integer id,
+            @RequestHeader(required = false) String requestId
+    ) {
+        if (requestId != null) {
+            log.info("RequestID was: " + requestId);
+        }
         String song = database.get(id);
         if (song == null) {
             return ResponseEntity.notFound().build();
