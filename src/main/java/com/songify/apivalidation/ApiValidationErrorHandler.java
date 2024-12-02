f084a1dec90a32d871ddfc1588db241697be755e
Radosław Lichtarski <lichtarski.radek@gmail.com>
2024-12-02 20:04:00 +0100

    better error handling

--- src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
+++ src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
@@ -3,24 +3,25 @@ package com.songify.apivalidation;
 import com.songify.song.infrastructure.controller.SongRestController;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice(assignableTypes = SongRestController.class)
+@RestControllerAdvice(assignableTypes = SongRestController.class)
 public class ApiValidationErrorHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiValidationErrorResponseDto handleValidationException(MethodArgumentNotValidException exception) {
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiValidationErrorResponseDto> handleValidationException(MethodArgumentNotValidException exception) {
         List<String> errorsFromException = getErrorsFromException(exception);
-        return new ApiValidationErrorResponseDto(errorsFromException, HttpStatus.BAD_REQUEST);
+        ApiValidationErrorResponseDto apiValidationErrorResponseDto = new ApiValidationErrorResponseDto(errorsFromException, HttpStatus.BAD_REQUEST);
+        return ResponseEntity
+                .badRequest()
+                .header("nice", "header")
+                .body(apiValidationErrorResponseDto);
     }
 
     private List<String> getErrorsFromException(MethodArgumentNotValidException exception) {
