4de1c23949187a0d8c659528822e85eefb4c045e
Radosław Lichtarski <lichtarski.radek@gmail.com>
2025-03-03 21:36:32 +0100

    fix - csrf, fix migration for creating users table

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -3,33 +3,31 @@ package com.songify.infrastructure.security;
 import com.songify.domain.usercrud.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 class SecurityConfig {
 
     @Bean
     public UserDetailsManager userDetailsManager(UserRepository userRepository) {
-//        var manager = new InMemoryUserDetailsManager();
-//        var user1 = User.withUsername("Radek")
-//                .password("12345")
-//                .roles("USER")
-//                .build();
-//        var user2 = User.withUsername("Smith")
-//                .password("12345")
-//                .roles("USER", "ADMIN")
-//                .build();
-//        manager.createUser(user1);
-//        manager.createUser(user2);
-//        return manager;
         return new UserDetailsServiceImpl(userRepository, passwordEncoder());
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity
+                .build();
     }
 
 }
--- src/main/resources/db.migration/general/V18__create_table_users.sql
+++ src/main/resources/db.migration/general/V18__create_table_users.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE IF NOT EXISTS users_id_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE users
 (
-    id          BIGSERIAL PRIMARY KEY NOT NULL,
+    id          BIGINT PRIMARY KEY NOT NULL DEFAULT nextval('users_id_seq'),
     uuid        UUID DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
     created_on  TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     version     BIGINT DEFAULT 0 NOT NULL,
