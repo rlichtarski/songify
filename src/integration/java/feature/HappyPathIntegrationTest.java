998b818d1e84c77790be57e64cc8000da440f4d7
Radosław Lichtarski <lichtarski.radek@gmail.com>
2025-03-11 22:55:23 +0100

    integration tests for security

--- src/integration/java/feature/HappyPathIntegrationTest.java
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -1,11 +1,14 @@
 package feature;
 
 import com.songify.SongifyApplication;
+import com.songify.infrastructure.security.jwt.JwtAuthConverter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -16,6 +19,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -34,9 +39,36 @@ class HappyPathIntegrationTest {
     @Autowired
     public MockMvc mockMvc;
 
+    @Autowired
+    private JwtAuthConverter jwtAuthConverter;
+
     @DynamicPropertySource
     public static void propertyOverride(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    }
+
+    private Jwt createJwtAdmin() {
+        return Jwt.withTokenValue("123")
+                .claim("email", "rradzzio@gmail.com")
+                .header("alg", "none")
+                .build();
+    }
+
+    private JwtAuthenticationToken createJwtWithAdminRole() {
+        Jwt jwt = createJwtAdmin();
+        return jwtAuthConverter.convert(jwt);
+    }
+
+    private Jwt createJwtUser() {
+        return Jwt.withTokenValue("123")
+                .claim("email", "john@gmail.com")
+                .header("alg", "none")
+                .build();
+    }
+
+    private JwtAuthenticationToken createJwtWithUserRole() {
+        Jwt jwt = createJwtUser();
+        return jwtAuthConverter.convert(jwt);
     }
 
     @Test
@@ -46,7 +78,22 @@ class HappyPathIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.songs").isEmpty());
 
+        mockMvc.perform(get("/songs")
+                        .with(authentication(createJwtWithAdminRole()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.songs").isEmpty());
+
+        mockMvc.perform(post("/songs"))
+                        .andExpect(status().isUnauthorized());
+
         mockMvc.perform(post("/songs")
+                        .with(authentication(createJwtWithUserRole()))
+                ).andExpect(status().isForbidden());
+
+        mockMvc.perform(post("/songs")
+//                .with(jwt().authorities(() -> "ROLE_ADMIN"))
+                .with(authentication(createJwtWithAdminRole()))
                 .content(
                 """
                     {
@@ -65,6 +112,7 @@ class HappyPathIntegrationTest {
         .andExpect(jsonPath("$.song.genre.name", is("default")));
 
         mockMvc.perform(post("/songs")
+                .with(jwt().authorities(() -> "ROLE_ADMIN"))
                 .content(
                 """
                     {
@@ -89,6 +137,7 @@ class HappyPathIntegrationTest {
                 .andExpect(jsonPath("$.genres[0].name", is("default")));
 
         mockMvc.perform(post("/genres")
+                        .with(jwt().authorities(() -> "ROLE_ADMIN"))
                         .content("""
                                 {
                                   "name": "Rap"
@@ -106,6 +155,7 @@ class HappyPathIntegrationTest {
                 .andExpect(jsonPath("$.song.genre.name", is("default")));
 
         mockMvc.perform(put("/songs/1/genres/2")
+                .with(jwt().authorities(() -> "ROLE_ADMIN"))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", is("updated")));
@@ -121,6 +171,7 @@ class HappyPathIntegrationTest {
                 .andExpect(jsonPath("$.albums").isEmpty());
 
         mockMvc.perform(post("/albums")
+                        .with(jwt().authorities(() -> "ROLE_ADMIN"))
                         .content("""
                                 {
                                   "songsId": [
@@ -143,6 +194,7 @@ class HappyPathIntegrationTest {
                 .andExpect(jsonPath("$.httpStatus", is("NOT_FOUND")));
 
         mockMvc.perform(post("/artists")
+                        .with(jwt().authorities(() -> "ROLE_ADMIN"))
                         .content("""
                                 {
                                   "name": "Eminem"
@@ -154,6 +206,7 @@ class HappyPathIntegrationTest {
                 .andExpect(jsonPath("$.name", is("Eminem")));
 
         mockMvc.perform(put("/artists/1/album/1")
+                        .with(jwt().authorities(() -> "ROLE_ADMIN"))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", is("Probably assigned artist to album!")));
@@ -165,6 +218,7 @@ class HappyPathIntegrationTest {
                 .andExpect(jsonPath("$.artists[*].id", containsInAnyOrder(1)));
 
         mockMvc.perform(put("/albums/1/songs/2")
+                        .with(jwt().authorities(() -> "ROLE_ADMIN"))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(1)))
---
+++ src/main/resources/db.migration/integration/V20__insert_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, password, authorities, enabled)
+VALUES
+    ('rradzzio@gmail.com', '$2a$10$3Igok6v2alSppZmfVwN/WuuaWtcvarQyHAnigj1NoEuG4REFA.jS.', '{ROLE_ADMIN, ROLE_USER}', true),
+    ('john@gmail.com', '$2a$10$mA2mB2gpw3t7HVorfyxewuNl18pwagodprUa5qIB/g2PXMdhmHOPu', '{ROLE_USER}', true);
